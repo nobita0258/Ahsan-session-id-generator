72c492bb585e9638d5dfb11c3e1dda5da138d64b
nobita0258 <ahsanali323245@gmail.com>
2026-02-22 09:12:39 +0500

    Change newsletter and session ID from ARSLAN-MD to AHSAN-MD

--- pair.js
+++ pair.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
                     const credsPath = path.join(tempDir, 'creds.json');
                     const sessionData = fs.readFileSync(credsPath, 'utf8');
                     const base64 = Buffer.from(sessionData).toString('base64');
-                    const sessionId = "ARSLAN-MD~" + base64;
+                    const sessionId = "AHSAN-MD" + base64;
 
                     await sock.sendMessage(sock.user.id, { text: sessionId });
 
@@ -76,7 +76,7 @@ router.get('/', async (req, res) => {
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
                                 newsletterJid: "120363348739987203@newsletter",
-                                newsletterName: "ARSLAN-MD",
+                                newsletterName: "AHSAN-MD",
                                 serverMessageId: 143
                             }
                         }
